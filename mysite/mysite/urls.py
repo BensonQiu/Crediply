d5b4c297d7ad6cd471b7177e6d5db3bb4ba04f58
BensonQiu <qiu.benson@gmail.com>
2016-02-05 23:41:34 -0500

    Add very barebones feature to create a meeting

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
    	url(r'^$', 'timeismoney.views.home', name='home'),
-    url(r'^createEvent$', 'timeismoney.views.createEvent', name='createEvent'),
+    url(r'^createMeeting$', 'timeismoney.views.createMeeting', name='createMeeting'),
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name':'timeismoney/login.html'}, name='login'),
     url(r'^logout$', 'django.contrib.auth.views.logout_then_login', name='logout'),
     url(r'^register$', 'timeismoney.views.register', name='register'),
--- mysite/timeismoney/models.py
+++ mysite/timeismoney/models.py
@@ -2,3 +2,10 @@ from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
 from django.db import models
+
+class Meeting(models.Model):
+	meetingName = models.CharField(max_length=100)
+	dateAndTime = models.DateTimeField()
+	location = models.CharField(max_length=100)
+	attendees = models.CharField(max_length=200)
+	# acceptStatus = models.BooleanField()
